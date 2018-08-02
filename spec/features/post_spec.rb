a14a0f66c714cafaf051795a80d8dc8de39881d6
Veronica Bustabad <vbustabad@gmail.com>
2018-08-02 02:11:39 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 describe 'new post' do
 
+  ...
+
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
 end
